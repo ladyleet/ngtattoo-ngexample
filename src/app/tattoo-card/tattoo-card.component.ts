1f1acfa4526c4ae556da432ae3d4097734783cff
Tracy Lee <tracy@wingman.events>
2016-07-13 04:45:18 -0700

    another test run

--- src/app/tattoo-card/tattoo-card.component.ts
+++ src/app/tattoo-card/tattoo-card.component.ts
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['tattoo-card.component.css'],
   template: `
   <div class="row">
-    <div class="col s12 m6 l3">
+    <div *ngFor="let tattoo of tattoos" class="col s12 m6 l3">
      <div class="card">
        <div class="card-image">
-         <img src="/img/ngtattoo-11.jpg">
-         <span class="card-title">link to twitter could go here?</span>
+         <img src="/img/ngtattoo-{{tattoo.number}}.jpg">
+         <span class="card-title">{{tattoo.twitter}}</span>
        </div>
      </div>
     </div>
@@ -19,6 +19,21 @@ import { Component, OnInit } from '@angular/core';
   `
 })
 export class TattooCardComponent implements OnInit {
+
+  tattoos: Object[] = [
+    { number: '1', twitter: "@jakeherringbone"},
+    { number: '2', twitter: "@synalx"},
+    { number: '3', twitter: "@benlesh"},
+    { number: '4', twitter: "@samccone"},
+    { number: '5', twitter: "@ladyleet"},
+    { number: '6', twitter: "@gerardsans"},
+    { number: '7', twitter: "@aysegulyonet"},
+    { number: '8', twitter: "@gdi2290"},
+    { number: '9', twitter: "@thejameskyle"},
+    { number: '10', twitter: "@wnodom"},
+    { number: '11', twitter: "@pascalprecht"},
+    { number: '12', twitter: "@urishaked"}
+  ];
 
   constructor() {}
 
