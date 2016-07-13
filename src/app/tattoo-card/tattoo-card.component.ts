f8279c5dc78317f25080ac5ffac010440837512f
Tracy Lee <tracy@wingman.events>
2016-07-13 01:29:46 -0700

    add a row class

--- src/app/tattoo-card/tattoo-card.component.ts
+++ src/app/tattoo-card/tattoo-card.component.ts
@@ -6,13 +6,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: 'tattoo-card.component.html',
   styleUrls: ['tattoo-card.component.css'],
   template: `
-  <div class="col s12 m6 l3">
-   <div class="card">
-     <div class="card-image">
-       <img src="/img/ngtattoo-11.jpg">
-       <span class="card-title">link to twitter could go here?</span>
+  <div class="row">
+    <div class="col s12 m6 l3">
+     <div class="card">
+       <div class="card-image">
+         <img src="/img/ngtattoo-11.jpg">
+         <span class="card-title">link to twitter could go here?</span>
+       </div>
      </div>
-   </div>
+    </div>
   </div>
   `
 })
