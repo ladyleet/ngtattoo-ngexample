6463c9b40406afe53b27c3a2b248cd0fbb307bd2
Tracy Lee <tracy@wingman.events>
2016-08-04 12:37:11 -0700

    2nd to last run

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,26 @@
 import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
+  directives: [ ROUTER_DIRECTIVES ],
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
   template: `
     <nav>
       <div class="nav-wrapper red">
-        <a href="#" class="brand-logo">ng-tattoo</a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><a href="#">temp</a></li>
-          <li><a href="#">temp</a></li>
-          <li><a href="#">temp</a></li>
+        <a href="#" class="brand-logo center">ng-tattoo</a>
+        <ul id="nav-mobile" class="right">
+          <li><a routerLink="/tattooed">Tattooed</a></li>
+          <li><a routerLink="/get-tattooed">Get Tattooed</a></li>
+          <li><a routerLink="/about">About</a></li>
         </ul>
       </div>
     </nav>
+    <div>
+      <router-outlet></router-outlet>
+    </div>
   `
 })
 export class AppComponent {
