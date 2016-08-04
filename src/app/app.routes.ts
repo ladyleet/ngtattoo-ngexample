dcb98bdb6a6de8771f514faff1729eb28f3f6c6b
Tracy Lee <tracy@wingman.events>
2016-08-04 08:15:07 -0700

    raleigh first run with forms success!

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { TattooCardComponent } from './tattoo-card';
+import { AboutPageComponent } from './about-page';
+import { GetTattooedComponent } from './get-tattooed';
+
+export const AppRoutes = [
+
+{ path: '', component: TattooCardComponent },
+{ path: 'tattooed', component: TattooCardComponent },
+{ path: 'about', component: AboutPageComponent },
+{ path: 'get-tattooed', component: GetTattooedComponent }
+];
--- src/app/get-tattooed/get-tattooed.component.ts
+++ src/app/get-tattooed/get-tattooed.component.ts
@@ -1,16 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Submissions } from './submissions.model';
 
 @Component({
   moduleId: module.id,
   selector: 'app-get-tattooed',
   templateUrl: 'get-tattooed.component.html',
-  styleUrls: ['get-tattooed.component.css']
+  styleUrls: ['get-tattooed.component.css'],
+  template: `
+  <div class="row margin-top-50">
+    <form #myForm="ngForm" (ngSubmit) = "submitForm(myForm)" class="col s12">
+      <div class="row">
+        <div class="input-field col s6">
+          <input [(ngModel)] = "submissions.firstName" id="first_name" name="first_name" type="text" class="validate">
+          <label for="first_name">First Name</label>
+        </div>
+        <div class="input-field col s6">
+          <input [(ngModel)] = "submissions.lastName" id="last_name" name="last_name" type="text" class="validate">
+          <label for="last_name">Last Name</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input [(ngModel)] = "submissions.email" id="email" type="email" name="email" class="validate">
+          <label for="email">Email</label>
+        </div>
+      </div>
+      <div class="row">
+      <button class="btn waves-effect waves-light blue" type="submit" name="action">Submit</button>
+      </div>
+    </form>
+  </div>
+  `
 })
 export class GetTattooedComponent implements OnInit {
+
+  submissions: Submissions = { firstName: '', lastName: '', email: '' }
 
   constructor() {}
 
   ngOnInit() {
   }
+
+  submitForm(myForm:NgForm) {
+    alert(JSON.stringify(myForm.value));
+ }
 
 }
