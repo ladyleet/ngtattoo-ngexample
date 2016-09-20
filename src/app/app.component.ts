79ab88dc8fe4c016c29bf655b950f3464bf63ac4
Tracy Lee <tracy@wingman.events>
2016-09-20 08:48:00 -0400

    save wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.css'],
   template: `
     <nav>
-      <div class="nav-wrapper red">
-        <a href="#" class="brand-logo">ng-tattoo</a>
+      <div class="nav-wrapper light-blue">
+        <a href="#" class="brand-logo center"><img src="/img/angular.png"/></a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
           <li><a href="#">temp</a></li>
           <li><a href="#">temp</a></li>
---
+++ src/app/routes.spec.ts
@@ -0,0 +1,15 @@
+/* tslint:disable:no-unused-variable */
+
+import {
+  beforeEach, beforeEachProviders,
+  describe, xdescribe,
+  expect, it, xit,
+  async, inject
+} from '@angular/core/testing';
+import {Routes} from './routes';
+
+describe('Routes', () => {
+  it('should create an instance', () => {
+    expect(new Routes()).toBeTruthy();
+  });
+});
